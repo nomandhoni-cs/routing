e11442d46111dfe336993224e1d22226a6bad657
nomandhoni-cs <nomandhoni@yahoo.com>
2022-12-22 06:20:06 +0600

    Practing reducer

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useReducer } from 'react'
 
 const Home = () => {
+  const initialState = {count: 0}
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return {count: state.count + 1}
+      case 'DECREMENT':
+        return {count: state.count - 1}
+      default:
+    }
+  }
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
+    <>
     <h4>Home Page</h4>
+    <button onClick={() => dispatch({type: 'INCREMENT'})}>Increase</button>
+    <h2>{state.count}</h2>
+    <button onClick={() => dispatch({type: 'DECREMENT'})}>Decrease</button>
+    </>
+
+
   )
 }
 
